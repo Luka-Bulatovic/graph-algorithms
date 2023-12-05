d8c56029dde1666aac102acdc63bc2c0389009fd
Luka-Bulatovic <luka.games94@gmail.com>
2023-12-05 08:10:55 +0100

    GR-30 Attempt to fix fullscreen

--- GraphAlgorithms.Web/wwwroot/js/GraphCanvas.js
+++ GraphAlgorithms.Web/wwwroot/js/GraphCanvas.js
@@ -172,7 +172,7 @@ var GraphCanvas = new function () {
             viewDataObj.network.fit();
         }
         else {
-            let container = viewDataObj.canvasContainer.get(0);
+            let container = viewDataObj.graphAndStatusBarContainer.get(0);
 
             if (container.requestFullscreen) {
                 container.requestFullscreen();
