5a23fc7848bd288159e61754e140ad98c80230ca
Luka-Bulatovic <luka.games94@gmail.com>
2023-12-07 09:39:48 +0100

    Added Partial view for ActionCards

---
+++ GraphAlgorithms.Web/Models/ActionCardModel.cs
@@ -0,0 +1,15 @@
+﻿using GraphAlgorithms.Service.DTO;
+
+namespace GraphAlgorithms.Web.Models
+{
+    public class ActionCardModel
+    {
+        private ActionDTO action;
+        public ActionDTO Action => action;
+
+        public ActionCardModel(ActionDTO action)
+        {
+            this.action = action;
+        }
+    }
+}
