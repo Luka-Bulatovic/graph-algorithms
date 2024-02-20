69c0b7734515006cab77788b450e4b26082e0a81
Luka-Bulatovic <luka.games94@gmail.com>
2024-02-20 10:35:41 +0100

    GR-53 Fixed bugs when editing existing Graphs

--- GraphAlgorithms.Service/Services/GraphDrawingService.cs
+++ GraphAlgorithms.Service/Services/GraphDrawingService.cs
@@ -14,11 +14,13 @@ namespace GraphAlgorithms.Service.Services
     public class GraphDrawingService : IGraphDrawingService
     {
         private readonly IGraphRepository graphRepository;
+        private readonly IGraphClassRepository graphClassRepository;
         private readonly IGraphConverter graphConverter;
 
-        public GraphDrawingService(IGraphRepository graphRepository, IGraphConverter graphConverter)
+        public GraphDrawingService(IGraphRepository graphRepository, IGraphClassRepository graphClassRepository, IGraphConverter graphConverter)
         {
             this.graphRepository = graphRepository;
+            this.graphClassRepository = graphClassRepository;
             this.graphConverter = graphConverter;
         }
 
@@ -33,25 +35,47 @@ namespace GraphAlgorithms.Service.Services
 
         public async Task<GraphDTO> StoreGraph(GraphDrawingUpdateDTO graphDTO/*, int ActionTypeID = 1*/)
         {
-            GraphEntity graphEntity = await graphConverter.GetGraphEntityFromGraphDrawingUpdateDTO(graphDTO);
+            GraphEntity graphEntity = null;
 
-            // We probably don't need ActionTypeID as this should always be for drawing, so enum value is enough
-
-            // Create an ActionEntity and set its properties
-            var actionEntity = new ActionEntity
+            if (graphDTO.id == 0)
             {
-                ActionTypeID = (int)ActionTypeEnum.Draw,
-                CreatedByID = 0, // Set the creator's ID,
-                CreatedDate = DateTime.UtcNow
-            };
+                // Create new GraphEntity object with calculated Properties and Classes
+                graphEntity = await graphConverter.GetGraphEntityFromGraphDrawingUpdateDTO(graphDTO);
+                
+                // Create an ActionEntity and set its properties
+                var actionEntity = new ActionEntity
+                {
+                    ActionTypeID = (int)ActionTypeEnum.Draw,
+                    CreatedByID = 0, // Set the creator's ID,
+                    CreatedDate = DateTime.UtcNow
+                };
 
-            // Associate the ActionEntity with the GraphEntity
-            graphEntity.Action = actionEntity;
+                // Associate the ActionEntity with the GraphEntity
+                graphEntity.Action = actionEntity;
+            }
+            else
+            {
+                graphEntity = await graphRepository.GetByIdAsync(graphDTO.id);
 
-            // TODO: Add some stuff here that should be passed as parameters to this method
-            // That stuff, such as ActionTypeID etc, should be added to graphEntity before persisting it
+                Graph graph = graphConverter.GetGraphFromGraphDrawingUpdateDTO(graphDTO, calculateProperties: true, calculateClasses: true);
 
-            // Save GraphEntity (along with the associated ActionEntity)
+                // Update GraphEntity object
+                graphEntity.Order = graph.Nodes.Count;
+                graphEntity.Size = graph.Edges.Count;
+                graphEntity.DataXML = GraphEvaluator.GetGraphMLForGraph(graph);
+                graphEntity.WienerIndex = graph.GraphProperties.WienerIndex;
+
+                // Update its GraphClasses
+                graphEntity.GraphClasses.Clear();
+                
+                List<int> graphClassIDs = graph.GraphClasses.Select(gc => (int)gc).ToList();
+                List<GraphClassEntity> graphClassEntities = await graphClassRepository.GetGraphClassesByIDsAsync(graphClassIDs);
+                
+                foreach(var graphClassEntity in graphClassEntities)
+                    graphEntity.GraphClasses.Add(graphClassEntity);
+            }
+
+            // Save GraphEntity (along with the associated entities)
             var storedGraphEntity = await graphRepository.SaveAsync(graphEntity);
 
             return graphConverter.GetGraphDTOFromGraphEntity(storedGraphEntity);
